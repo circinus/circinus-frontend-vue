91a353dc42c48e5598e205b94fd66dc2b022bb94
Raizer <raizer@meteor.nl>
2020-11-05 02:51:05 +0100

    Added middlewarePipeline

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,18 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
+import store from '../store'
+
+import guest from './middleware/guest'
+import auth from './middleware/auth'
+import middlewarePipeline from './middlewarePipeline'
+
 import Home from '../views/Home'
 import Register from '../views/Register'
 
-import store from '../store'
-
 Vue.use(Router);
 
-let router = new Router({
+const router = new Router({
   mode: 'history',
   routes: [
     {
@@ -19,11 +23,21 @@ let router = new Router({
     {
       path: '/register',
       name: 'register',
-      component: Register
+      component: Register,
+        meta: {
+            middleware: [
+                guest
+            ]
+        }
     },
     {
       path: '/hotel',
       name: 'hotel',
+        meta: {
+            middleware: [
+                auth
+            ]
+        },
       beforeEnter: (to, from, next) => {
           if(!store.getters['client/loaded']) {
               store.dispatch('client/setstate', true)
@@ -35,16 +49,34 @@ let router = new Router({
       path: '/dashboard',
       name: 'dashboard',
       component: Home,
-      beforeEnter: (to, from, next) => {
-          if(!store.getters['auth/authenticated']) {
-              return next({
-                  name: 'register'
-              })
-          }
-          next()
-      }
+        meta: {
+            middleware: [
+                auth
+            ]
+        }
     }
   ]
 });
+
+router.beforeEach((to, from, next) => {
+    if (!to.meta.middleware) {
+        return next()
+    }
+    const middleware = to.meta.middleware
+
+    const context = {
+        to,
+        from,
+        next,
+        store
+    }
+
+
+    return middleware[0]({
+        ...context,
+        next: middlewarePipeline(context, middleware, 1)
+    })
+
+})
 
 export default router;
