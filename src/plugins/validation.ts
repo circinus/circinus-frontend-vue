9e1340d600b70c54d04bb020200f2e8733f4eb4f
Eran Machiels <dev@eranmachiels.nl>
2021-02-14 11:07:43 +0100

    Set validator rules

--- src/plugins/validation.ts
+++ src/plugins/validation.ts
@@ -1,9 +1,12 @@
 import Vue from 'vue';
 import { extend, ValidationObserver, ValidationProvider } from 'vee-validate'
-import { min, required } from 'vee-validate/dist/rules';
+import { confirmed, max, min, regex, required } from 'vee-validate/dist/rules';
 
 Vue.component('ValidationProvider', ValidationProvider);
 Vue.component('ValidationObserver', ValidationObserver);
 
 extend('required', required);
 extend('min', min);
+extend('max', max);
+extend('regex', regex);
+extend('confirmed', confirmed);
