d9c7052ec4a4607c6ad951dcf7e59b1599b9d586
Eran Machiels <dev@eranmachiels.nl>
2021-02-21 20:53:24 +0100

    Changed locale

--- src/views/Me/Settings/Security.vue
+++ src/views/Me/Settings/Security.vue
@@ -4,58 +4,61 @@
             <div class="d-flex flex-row mb-4">
                 <i class="icon--settings mr-2"></i>
                 <div class="d-inline-flex flex-column justify-content-center">
-                    <span class="section-title">Wachtwoord</span>
+                    <span class="section-title" v-t="'layout.component.settings.security.password.title'" />
                 </div>
             </div>
             <b-card class="card--content">
                 <ValidationObserver v-slot="{ handleSubmit }" ref="passwordObserver">
-                    <form
-                        @submit.prevent="handleSubmit(submitPassword)"
-                        class="d-flex flex-column"
-                    >
+                    <form @submit.prevent="handleSubmit(submitPassword)">
                         <ValidationProvider name="password" rules="required" v-slot="{ classes }">
-                            <strong>Wat is je huidige wachtwoord?</strong>
+                            <strong v-t="'layout.component.settings.security.password.current_password'" />
                             <div class="form-label-group">
                                 <input
                                     type="password"
                                     class="form-control"
                                     :class="classes"
                                     id="password"
-                                    placeholder="Je huidige wachtwoord"
+                                    :placeholder="$t('layout.component.settings.security.password.current_password_label')"
                                     v-model="passwordSettings.password"
                                 />
-                                <label for="password">Je huidige wachtwoord</label>
+                                <label for="password" v-t="'layout.component.settings.security.password.current_password_label'" />
                             </div>
                         </ValidationProvider>
                         <ValidationProvider name="new_password" rules="required" v-slot="{ classes }">
-                            <strong>Wat wordt je nieuwe wachtwoord?</strong>
+                            <strong v-t="'layout.component.settings.security.password.new_password'" />
                             <div class="form-label-group">
                                 <input
                                     type="password"
                                     class="form-control"
                                     :class="classes"
                                     id="new_password"
-                                    placeholder="Nieuwe wachtwoord"
+                                    :placeholder="$t('layout.component.settings.security.password.new_password_label')"
                                     v-model="passwordSettings.new_password"
                                 />
-                                <label for="new_password">Nieuw wachtwoord</label>
+                                <label for="new_password" v-t="'layout.component.settings.security.password.new_password_label'" />
                             </div>
                         </ValidationProvider>
                         <ValidationProvider name="new_password_confirmation" rules="required|confirmed:new_password" v-slot="{ classes }">
-                            <strong>Je nieuwe wachtwoord nogmaals, ter controle</strong>
+                            <strong v-t="'layout.component.settings.security.password.new_password_confirmation'" />
                             <div class="form-label-group">
                                 <input
                                     type="password"
                                     class="form-control"
                                     :class="classes"
                                     id="new_password_confirmation"
-                                    placeholder="Nieuwe wachtwoord opnieuw"
+                                    :placeholder="$t('layout.component.settings.security.password.new_password_confirmation_label')"
                                     v-model="passwordSettings.new_password_confirmation"
                                 />
-                                <label for="new_password_confirmation">Nieuwe wachtwoord opnieuw</label>
+                                <label for="new_password_confirmation" v-t="'layout.component.settings.security.password.new_password_confirmation_label'" />
                             </div>
                         </ValidationProvider>
-                        <button type="submit" class="btn btn-primary mt-4">Verander wachtwoord</button>
+                        <div class="d-flex">
+                            <button
+                            type="submit"
+                            class="ml-auto btn btn-primary mt-4"
+                            v-t="'layout.component.settings.security.password.button_save'"
+                            />
+                        </div>
                     </form>
                 </ValidationObserver>
             </b-card>
@@ -64,45 +67,50 @@
             <div class="d-flex flex-row mb-4">
                 <i class="icon--settings mr-2"></i>
                 <div class="d-inline-flex flex-column justify-content-center">
-                    <span class="section-title">E-mailadres</span>
+                    <span class="section-title" v-t="'layout.component.settings.security.email.title'" />
                 </div>
             </div>
             <b-card class="card--content">
-                <i class="mb-4 d-block">Je huidge e-mailadres is {{ authModule.user.mail }} </i>
+                <i class="mb-4 d-block">
+                    {{ $t('layout.component.settings.security.email.current_email', { email: authModule.user.mail }) }}
+                </i>
                 <ValidationObserver v-slot="{ handleSubmit }" ref="emailObserver">
-                    <form
-                        @submit.prevent="handleSubmit(submitEmail)"
-                        class="d-flex flex-column"
-                    >
+                    <form @submit.prevent="handleSubmit(submitEmail)">
                         <ValidationProvider name="email_password" rules="required" v-slot="{ classes }">
-                            <strong>Wat is je wachtwoord?</strong>
+                            <strong v-t="'layout.component.settings.security.email.current_password'" />
                             <div class="form-label-group">
                                 <input
                                     type="password"
                                     class="form-control"
                                     :class="classes"
                                     id="email_password"
-                                    placeholder="Je wachtwoord"
+                                    :placeholder="$t('layout.component.settings.security.email.current_password_label')"
                                     v-model="emailSettings.password"
                                 />
-                                <label for="email_password">Je wachtwoord</label>
+                                <label for="email_password" v-t="'layout.component.settings.security.email.current_password_label'" />
                             </div>
                         </ValidationProvider>
                         <ValidationProvider name="new_email" rules="required" v-slot="{ classes }">
-                            <strong>Wat wordt je nieuwe e-mailadres?</strong>
+                            <strong v-t="'layout.component.settings.security.email.new_email'" />
                             <div class="form-label-group">
                                 <input
                                     type="text"
                                     class="form-control"
                                     :class="classes"
                                     id="new_email"
-                                    placeholder="Nieuwe wachtwoord"
+                                    :placeholder="$t('layout.component.settings.security.email.new_email_label')"
                                     v-model="emailSettings.email"
                                 />
-                                <label for="new_email">Nieuw e-mailadres</label>
+                                <label for="new_email" v-t="'layout.component.settings.security.email.new_email_label'" />
                             </div>
                         </ValidationProvider>
-                        <button type="submit" class="btn btn-primary mt-4">Verander e-mailadres</button>
+                        <div class="d-flex">
+                            <button
+                                type="submit"
+                                class="ml-auto btn btn-primary mt-4"
+                                v-t="'layout.component.settings.security.email.button_save'"
+                            />
+                        </div>
                     </form>
                 </ValidationObserver>
             </b-card>
