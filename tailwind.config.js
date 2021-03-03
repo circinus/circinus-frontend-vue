cbd1d236a0d2f188cd0527c157c999ef6df8bc27
Eran Machiels <dev@eranmachiels.nl>
2021-03-03 12:58:44 +0100

    Added tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    purge: ['./public/index.html', './src/**/*.{vue,ts}'],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {}
+    },
+    variants: {
+        extend: {}
+    },
+    plugins: []
+};
